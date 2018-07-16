09c96064199eb27ad3cf3f01e6dcc49f3b311bd1
H-Plus-Time <hydrogenplustime@gmail.com>
2018-07-17 00:47:53 +1000

    Update build-run-remarkable.sh

--- build-run-remarkable.sh
+++ build-run-remarkable.sh
@@ -4,7 +4,7 @@ set -ex
 ./build.sh
 ./dist.sh
 
-DEVICE_IP="192.168.86.179"
+DEVICE_IP="10.11.99.1"
 ssh root@$DEVICE_IP 'systemctl stop xochitl || true ; killall -9 plato || true; killall -9 draft || true; systemctl stop xochitl || true'
 scp -r ./dist root@$DEVICE_IP:~/
 ssh root@$DEVICE_IP 'cd dist && RUST_BACKTRACE=1 PRODUCT=remarkable LD_LIBRARY_PATH=./libs ./plato ' "$@"
